e2bf010788ba786ac5aab2c5854ccc4a1a90f8b4
Aaron Probus <aprobus15@gmail.com>
2012-06-21 19:23:23 -0400

    Refactored code to be more readable

--- public/javascripts/current.js
+++ public/javascripts/current.js
@@ -1,3 +1,44 @@
+//------------------------------- Data Models -------------------------------
+
+/**
+ * Data model of a Ret user
+ * @type {*}
+ */
+App.personModel = Ember.Object.extend({
+    name: null,
+    userName: null,
+    expenses: [],
+
+    paid: function() {
+        var totalExpenses = 0.0;
+        var expenses = this.get('expenses');
+        for (var i = 0; i < expenses.length; i++) {
+            totalExpenses += expenses[i].amount;
+        }
+
+        return totalExpenses;
+    }.property('expenses.@each'),
+
+    paidString: function () {
+        return this.get('paid').toFixed(2);
+    }.property('paid'),
+
+    owe: 0.0,
+
+    absOweString: function () {
+        return Math.abs(this.get('owe').toFixed(2));
+    }.property('owe'),
+
+    inDebt: function () {
+        return this.get('owe') < 0;
+    }.property('owe')
+});
+
+//------------------------------- Controllers -------------------------------
+
+/**
+ * Controls the list of users
+ */
 App.peopleController = Ember.ArrayController.create({
     content: [],
     names: function () {
@@ -31,115 +72,30 @@ App.peopleController = Ember.ArrayController.create({
         return this.get('content').length;
     }.property('content.@each'),
 
-    peopleChanged: drawGraph.observes('content.@each.paid'),
+    onExpensesChanged: function () {
+        var content = this.get('content');
+        var expensePerPerson = this.get('totalPaid') / content.length;
 
-    addExpense: function (userName, expense) {
-        var user = this.get('content').findProperty('userName', userName);
+        for (var i = 0; i < content.length; i++) {
+            var person = content[i];
+            var owe = person.get('paid') - expensePerPerson;
+            person.set('owe', owe);
+        }
+
+        drawGraph();
+    }.observes('content.@each.paid'),
+
+    addExpense: function (formData) {
+        var user = this.get('content').findProperty('userName', formData.payer);
         if (user) {
-            user.get('expenses').pushObject(expense);
+            user.get('expenses').pushObject(formData.expense);
         }
     }
 });
 
-App.peopleController.addObserver('@each.paid', function () {
-    var content = this.get('content');
-    var expensePerPerson = this.get('totalPaid') / content.length;
-
-    for (var i = 0; i < content.length; i++) {
-        var person = content[i];
-        var owe = person.get('paid') - expensePerPerson;
-        person.set('owe', owe);
-    }
-});
-
-App.personController = Ember.Object.extend({
-    name: null,
-    userName: null,
-    expenses: [],
-
-    paid: function() {
-        var totalExpenses = 0.0;
-        var expenses = this.get('expenses');
-        for (var i = 0; i < expenses.length; i++) {
-            totalExpenses += expenses[i].amount;
-        }
-
-        return totalExpenses;
-    }.property('expenses.@each'),
-
-    paidString: function () {
-      return this.get('paid').toFixed(2);
-    }.property('paid'),
-
-    owe: 0.0,
-
-    absOweString: function () {
-        return Math.abs(this.get('owe').toFixed(2));
-    }.property('owe'),
-
-    inDebt: function () {
-        return this.get('owe') < 0;
-    }.property('owe')
-});
-
-App.summaryTableView = Ember.View.extend({
-    peopleBinding: 'App.peopleController.content'
-});
-
-App.expenseTableView = Ember.View.extend({
-   content: function () {
-       var people = App.peopleController.get('content');
-
-       var expenses = [];
-       for (var i = 0; i < people.length; i++) {
-           var personExpenses = getUpdatedExpensesForPerson(people[i]);
-           expenses = expenses.concat(personExpenses);
-       }
-
-       var sortedExpenses = expenses.sort(function (expense1, expense2) {
-           if (expense1.timeStamp > expense2.timeStamp) return -1;
-           if (expense1.timeStamp < expense2.timeStamp) return 1;
-           return 0;
-       });
-
-       return sortedExpenses;
-
-       function getUpdatedExpensesForPerson (person) {
-           var personalExpenses = person.get('expenses');
-           var updatedExpenses = personalExpenses.map(function (personalExpense) {
-              var updatedExpense = Ember.Object.create({
-                  timeStamp: new Date(personalExpense.timeStamp),
-                  timeStampString: function () {
-                      var timeStamp = this.get('timeStamp');
-                      var day = timeStamp.getDate();
-                      var month = timeStamp.getMonth() + 1;
-                      var year = timeStamp.getFullYear().toString().substring(2);
-
-                      return month + '/' + day + '/' + year;
-                  }.property('timeStamp'),
-                  user: person.name,
-                  amount: personalExpense.amount.toFixed(2),
-                  comment: personalExpense.comment
-              }) ;
-
-               return updatedExpense;
-           });
-
-           return updatedExpenses;
-       }
-   }.property('App.peopleController.content.@each.paid')
-});
-
-App.expenseFormView = Ember.View.extend({
-    attributeBindings: ['action'],
-    action: '#',
-
-    namesBinding: 'App.peopleController.names',
-    commentBinding: 'App.expenseForm.commentView.value',
-    selectedUserBinding: 'App.expenseForm.nameSelectorView.selection',
-    amountBinding: 'App.expenseForm.amountView.value'
-});
-
+/*
+  Represents the data used when adding an expense
+ */
 App.expenseFormController = Ember.Object.create({
     user: null,
     amount: null,
@@ -149,25 +105,146 @@ App.expenseFormController = Ember.Object.create({
         this.set('user', null);
         this.set('amount', null);
         this.set('comment', null);
+    },
+
+    getFormData: function () {
+        var expense = {
+          amount: parseInt(this.get('amount')),
+          comment: this.get('comment'),
+          timeStamp: new Date()
+        };
+
+        var formData = {
+            payer: this.get('user') ,
+            expense: expense
+        };
+
+        return formData;
+    },
+
+    getErrors: function () {
+        var formData = this.getFormData();
+        var errors = [];
+
+        if (!formData.payer) {
+            errors.push('Must specify a payer');
+        }
+
+        if (isNaN(formData.expense.amount)) {
+            errors.push('Amount is not a number!');
+        } else if (formData.expense.amount <= 0) {
+            errors.push('Amount paid must be positive');
+        }
+
+        if (!formData.expense.comment) {
+            errors.push('Add a brief comment on the expense');
+        }
+
+        return errors;
     }
 });
 
-App.expenseForm = Ember.Object.create({
+/*
+    List of current errors
+ */
+App.formAlertController = Ember.Object.create({
+    errors: null
+});
+
+//------------------------------- Views -------------------------------
+
+/*
+    View of the total amounts that people have paid/owe
+ */
+App.summaryTableView = Ember.View.extend({
+    peopleBinding: 'App.peopleController.content'
+});
+
+/*
+    View of the most recent expenses
+ */
+App.expenseTableView = Ember.View.extend({
+    content: function () {
+        var people = App.peopleController.get('content');
+
+        var expenses = [];
+        for (var i = 0; i < people.length; i++) {
+            var personExpenses = getUpdatedExpensesForPerson(people[i]);
+            expenses = expenses.concat(personExpenses);
+        }
+
+        var sortedExpenses = expenses.sort(function (expense1, expense2) {
+            if (expense1.timeStamp > expense2.timeStamp) return -1;
+            if (expense1.timeStamp < expense2.timeStamp) return 1;
+            return 0;
+        });
+
+        return sortedExpenses;
+
+        function getUpdatedExpensesForPerson (person) {
+            var personalExpenses = person.get('expenses');
+            var updatedExpenses = personalExpenses.map(function (personalExpense) {
+                var updatedExpense = Ember.Object.create({
+                    timeStamp: new Date(personalExpense.timeStamp),
+                    timeStampString: function () {
+                        var timeStamp = this.get('timeStamp');
+                        var day = timeStamp.getDate();
+                        var month = timeStamp.getMonth() + 1;
+                        var year = timeStamp.getFullYear().toString().substring(2);
+
+                        return month + '/' + day + '/' + year;
+                    }.property('timeStamp'),
+                    user: person.name,
+                    amount: personalExpense.amount.toFixed(2),
+                    comment: personalExpense.comment
+                }) ;
+
+                return updatedExpense;
+            });
+
+            return updatedExpenses;
+        }
+    }.property('App.peopleController.content.@each.paid')
+});
+
+/*
+    View of form to add expenses
+ */
+App.expenseFormView = Ember.View.extend({
+    attributeBindings: ['action'],
+    action: '#',
+
+    namesBinding: 'App.peopleController.names',
+    commentBinding: 'App.expenseFormControls.commentView.value',
+    selectedUserBinding: 'App.expenseFormControls.nameSelectorView.selection',
+    amountBinding: 'App.expenseFormControls.amountView.value'
+});
+
+/*
+    Required components of the expenseFormView
+ */
+App.expenseFormControls = Ember.Object.create({
+    /*
+        View to select which user to add expenses to
+     */
     nameSelectorView: Ember.Select.extend({
         attributeBindings: ['name'],
         name: 'payer',
 
         selectionBinding: 'App.expenseFormController.user',
         /*TODO: Find out what is going wrong here
-        optionLabelPathBinding: 'content.name',
-        optionValuePathBinding: 'content.key',
-        contentBinding: 'App.peopleController.content',*/
+         optionLabelPathBinding: 'content.name',
+         optionValuePathBinding: 'content.key',
+         contentBinding: 'App.peopleController.content',*/
 
         contentBinding: 'App.peopleController.userNames',
 
         prompt: "Select a User"
     }),
 
+    /*
+        View to add a comment about the expense
+     */
     commentView: Ember.TextField.extend({
         attributeBindings: ['placeHolder'],
         placeHolder: 'Comment',
@@ -175,6 +252,9 @@ App.expenseForm = Ember.Object.create({
         valueBinding: 'App.expenseFormController.comment'
     }),
 
+    /*
+        View to specify how much money was spent
+     */
     amountView: Ember.TextField.extend({
         attributeBindings: ['type', 'style'],
         type: 'number',
@@ -185,78 +265,60 @@ App.expenseForm = Ember.Object.create({
         valueBinding: 'App.expenseFormController.amount'
     }),
 
+    /*
+        View to submit the expense
+     */
     submitView: Ember.Button.extend({
         attributeBindings: ['type', 'value'],
         value: 'Submit',
         type: 'submit',
 
         click: function (event) {
-            var payer = App.expenseFormController.get('user');
-            var expense = {
-                timeStamp: new Date(),
-                amount: parseFloat(App.expenseFormController.get('amount')),
-                comment: App.expenseFormController.get('comment')
-            };
-
-            var validControl = {
-                payer: Boolean(payer),
-                amount: !isNaN(expense.amount) && expense.amount > 0,
-                comment: expense.comment
-            };
-
-            if (validControl.payer && validControl.amount && validControl.comment) {
-                var ajaxData = {
-                    amount: expense.amount,
-                    comment: expense.comment
-                };
-
-                jQuery.getJSON('/current/add/' + payer, ajaxData, function(post) {
-                    if (post && post.status === 200 && !post.error) {
-                        App.formAlertController.set('errors', null);
-                        App.peopleController.addExpense(payer, expense);
-                        App.expenseFormController.reset();
-                    } else if (post && post.error) {
-                        App.formAlertController.set('errors', [post.error]);
-                    }
-                });
-            } else {
-                var errors = [];
-                if (!validControl.payer) {
-                    errors.push('Invalid user selected');
-                }
-
-                if (!validControl.amount) {
-                    errors.push('Invalid amount entered');
-                }
-
-                if (!validControl.comment) {
-                    errors.push('Please enter a comment');
-                }
-
-                App.formAlertController.set('errors', errors);
+            var formErrors = App.expenseFormController.getErrors();
+            if (formErrors && formErrors.length > 0) {
+                App.formAlertController.set('errors', formErrors);
+                return;
             }
+
+            var formData = App.expenseFormController.getFormData();
+
+            var ajaxData = {
+                amount: formData.expense.amount,
+                comment: formData.expense.comment
+            };
+
+            jQuery.getJSON('/current/add/' + formData.payer, ajaxData, function(post) {
+                if (post && post.status === 200 && !post.error) {
+                    App.formAlertController.set('errors', null);
+                    App.peopleController.addExpense(formData);
+                    App.expenseFormController.reset();
+                } else if (post && post.error) {
+                    App.formAlertController.set('errors', [post.error]);
+                }
+            });
         }
     })
 });
 
-App.formAlertController = Ember.Object.create({
-    errors: null
-});
-
+/*
+    View of current errors
+ */
 App.formAlertView = Ember.View.extend({
-   attributeBindings: ['hasNoErrors:hidden'],
-   hasNoErrors: function () {
-       var errors = this.get('errors');
-       return Boolean(!errors || errors.length === 0);
-   }.property('errors'),
+    attributeBindings: ['hasNoErrors:hidden'],
+    hasNoErrors: function () {
+        var errors = this.get('errors');
+        return Boolean(!errors || errors.length === 0);
+    }.property('errors'),
 
-   errorsBinding: 'App.formAlertController.errors'
+    errorsBinding: 'App.formAlertController.errors'
 });
+
+//------------------------------- Other -------------------------------
 
 $(document).ready(function() {
     jQuery.getJSON('/current', function(post) {
         var emberPeople = post.map(function (person) {
-           return App.personController.create(person);
+           return App.personModel.create(person);
         });
         App.peopleController.set('content', emberPeople);
     });
