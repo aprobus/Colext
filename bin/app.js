3524ee9e43199fd4fd5ae585b03e6540067ac738
Aaron Probus <aprobus15@gmail.com>
2012-06-23 15:04:12 -0400

    Added config file support

--- bin/app.js
+++ bin/app.js
@@ -1,44 +1,52 @@
 var express = require('express');
 var nano = require('nano');
 var path = require('path');
+var configLoader = require('../lib/configLoader');
 
-var app = module.exports = express.createServer();
+configLoader.load(function (err, config) {
+    if (err || !config) {
+        console.error('Unable to load configuration!');
+        return;
+    }
 
-// Configuration
+    var app = module.exports = express.createServer();
 
-var viewsDir = path.join(__dirname, '..', 'views');
-var publicDir = path.join(__dirname, '..', 'public');
+    // Configuration
 
-app.configure(function(){
-  app.set('views', viewsDir);
-  app.set('view engine', 'ejs');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(publicDir));
-});
+    var viewsDir = path.join(__dirname, '..', 'views');
+    var publicDir = path.join(__dirname, '..', 'public');
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+    app.configure(function(){
+        app.set('views', viewsDir);
+        app.set('view engine', 'ejs');
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(app.router);
+        app.use(express.static(publicDir));
+    });
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
+    app.configure('development', function(){
+        app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    });
 
-var couchDB = nano('http://192.168.1.130:5984');
-var retUsers = couchDB.db.use('ret_users');
+    app.configure('production', function(){
+        app.use(express.errorHandler());
+    });
 
-// Routes
-var routes = {
-    main: require('./../routes/index'),
-    current: require('./../routes/current').create(retUsers)
-};
+    var couchDB = nano('http://' + config.couch.host + ':' + config.couch.port);
+    var retUsers = couchDB.db.use('ret_users');
 
-app.get('/', routes.main.index);
-app.get('/current', routes.current.index);
-app.get('/current/add/:userName', routes.current.add);
+    // Routes
+    var routes = {
+        main: require('./../routes/index'),
+        current: require('./../routes/current').create(retUsers)
+    };
 
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+    app.get('/', routes.main.index);
+    app.get('/current', routes.current.index);
+    app.get('/current/add/:userName', routes.current.add);
+
+    app.listen(config.server.port, function(){
+        console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+    });
 });
